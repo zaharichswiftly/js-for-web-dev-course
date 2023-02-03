375d07fda6f1007ce7f402a40de3997bdeba043b
raynelz <97597336+zaharichswiftly@users.noreply.github.com>
2023-02-04 02:58:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -34,3 +34,112 @@
 /* DOM */
 
 // прикреплю ссылкой
+
+/* Array methods */
+//
+// const cities = ['Moscow', 'Kazan', 'Ekaterinburg', 'Poltava', 'Krasnodar']
+//
+// const students = [
+// {
+// name: 'Zahar',
+// age: 18,
+// city: 'Moscow'
+// },
+// {
+// name: 'Ivan',
+// age: 20,
+// city: 'Paris'
+// },
+// {
+// name: 'Petro',
+// age: 19,
+// city: 'Madrid'
+// }
+// ]
+//
+/* for (циклы)*/
+
+// for (let i = 0; i < cities.length; i++) {
+// 	console.log(cities[i])
+// }
+
+/* map || foreach */
+
+// cities.forEach(city => console.log(city))
+
+// console.log(cities.map(city => city + '1'))
+
+/* filter */
+
+// console.log(students.filter(student => student.age >= 20))
+
+/* push & unshift */
+
+// cities.push('Berlin')
+// cities.unshift('Milan')
+// console.log(cities)
+
+/* slice */
+
+// console.log(cities.slice(1, 3))
+
+/* find */
+
+// console.log(students.find(student => student.name === 'Zahar'))
+
+/* reduce */
+//
+// const cart = [
+// {
+// id: 1,
+// name: 'iPhone 12',
+// price: 100000
+// },
+// {
+// id: 2,
+// name: 'iPhone 12 Pro',
+// price: 120000
+// },
+// {
+// id: 3,
+// name: 'iPhone 12 Pro Max',
+// price: 140000
+// }
+// ]
+//
+// console.log(cart.reduce((acc, product) => acc + product.price, 0))
+//
+
+/* some objects */
+
+// const person = {
+// name: 'Zahar',
+// age: 18,
+// city: 'Moscow'
+// }
+//
+// const dividend = {
+// profit: 100
+// }
+
+// Assign
+
+// console.log(Object.assign(person, dividend))
+
+/* Entries */
+
+// console.log(Object.entries(person)) <- превращает объект в  своеобразный массив
+
+/* Keys and values */
+
+// console.log(Object.keys(person)) <- выводит ключи
+// console.log(Object.values(person)) <- выводит значения
+
+/* Fetch */
+
+// document.querySelector('body').style.backgroundColor = '#1F1F28'
+//
+// fetch('https://jsonplaceholder.typicode.com/todos/2')
+// .then(json => console.log(json))
+// .then(response => response.json)
+//
